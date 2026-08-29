c8073049f67db356057859a7a48636d01bd4fd5a
israelsegado <israel072003@gmail.com>
2026-08-29 19:53:17 +0200

    CreateUser function updated

--- app/main.py
+++ app/main.py
@@ -1,61 +1,3 @@
-# print("EJERCICIO 1:\n")
-
-# from pydantic import BaseModel, ConfigDict, EmailStr, ConfigDict, Field, ValidationError
-# from typing import Optional
-
-# class Address(BaseModel):
-#     model_config = ConfigDict(frozen=True, extra="ignore")
-#     ciudad: str
-#     codigo_postal: int = Field(ge=10000, le=99999)
-
-# class User(BaseModel):
-#     model_config = ConfigDict(extra='forbid', str_strip_whitespace=True, frozen=True)
-
-#     email: EmailStr
-#     edad: int = Field(gt=18)
-#     nombre: str = Field(min_length=2)
-#     direccion: Optional[Address] = None
-
-# inf_ok = {"email": "ejemplo@gmail.com", "edad": 20, "nombre": "John"}
-# inf_bad = {"email": "jane", "edad": 15, "nombre": "Jane"}
-
-# user1 = User(email="ejemplo@gmail.com", edad=20, nombre="   John   ")
-# print(f"Nombre sin espacios: {user1.nombre}\n")
-
-# # Bloque errores de validación de datos de entrada -------------------
-# try:
-#     print("Probando a hacer las validaciones de datos de entrada válidos")
-#     User.model_validate(inf_ok)
-#     User.model_validate(inf_bad)
-    
-
-# except ValidationError as exc:
-#     for e in exc.errors():
-#         print(f"Campo: {e['loc']}")      
-#         print(f"Mensaje: {e['msg']}")
-#         print(f"Tipo: {e['type']}")      
-#         print(f"Input: {e['input']}")     
-#         print("---")
-# #---------------------------------------------------------
-
-# # Bloque prueba del atributo frozen ----------------------
-# try:
-#     print("\nProbando a hacer cambios en una clase con atributo frozen")
-#     user1.nombre = "Jane"
-
-# except ValidationError as exc:
-#     for e in exc.errors():
-#         print(f"Campo: {e['loc']}")      
-#         print(f"Mensaje: {e['msg']}")
-#         print(f"Tipo: {e['type']}")      
-#         print(f"Input: {e['input']}")     
-#         print("---")
-# #-----------------------------------------------------
-
-# print(f'{'='*100}')
-
-print("EJERCICIO 2:\n")
-
 from pydantic import BaseModel, ConfigDict, EmailStr, ConfigDict, Field, ValidationError
 from typing import Optional
 from datetime import datetime
@@ -94,9 +36,12 @@ def listUsers():
     return users
 
 #función para crear usuarios
-@app.post("/users")
+@app.post("/users", status_code=201, response_model=UserResponse)
 def createUser(user: UserCreate):
-    return user
+    new_id = max(u.id for u in users) + 1
+    new_user = UserResponse(id=new_id, userCreate=user, created_at=datetime.now())
+    users.append(new_user)
+    return new_user
 
 #función para mostrar en pantalla el usuario con el id seleccionado
 @app.get("/users/{user_id}")
